369c217c5e31c6521131362f6320767c2ea6110f
Justin O'Connor <juleoc@colostate.edu>
2021-05-12 21:33:24 -0600

    Whitespace for indent.

--- examples/step-79/step-79.cc
+++ examples/step-79/step-79.cc
@@ -226,7 +226,8 @@ namespace SAND
     void write_as_stl();
 
     std::set<typename Triangulation<dim>::cell_iterator>
-    find_relevant_neighbors(typename Triangulation<dim>::cell_iterator cell) const;
+    find_relevant_neighbors(
+      typename Triangulation<dim>::cell_iterator cell) const;
 
 
     // Most of the member variables are also standard. There are,
